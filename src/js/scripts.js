a0aa370568d1e8e9812ce2903c533239db8eeba0
Karly Hoffman <karlyhoffman@gmail.com>
2017-09-22 23:24:41 -0500

    About page — structure build + styling

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,14 @@
   'use strict';
 
   $(function () {
-    
+
+    var pathName = window.location.pathname;
+    if (pathName == '/about.html') {
+      $('header, footer').addClass('white-copy');
+    } else {
+      $('header, footer').removeClass('white-copy');
+    }
+
   });
 
 })(jQuery, window, document);
