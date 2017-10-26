18c889192ef6b06e316a13bef0e67a7845e685ca
Karly Hoffman <karlyhoffman@gmail.com>
2017-10-26 12:14:50 -0500

    Homepage (desktop) — image animations
    also updated temp images

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -11,10 +11,17 @@
     // Homepage project list
     var listItem = $('.project-list > li');
     $(listItem).mouseenter(function() {
+      // Navigation Animation
       $(listItem).each(function() {
         $( this ).removeClass('active');
       });
       $(this).addClass('active');
+      // Image Change
+      for (var i = 1; i < listItem.length + 1; i++) {
+        if ($(this).hasClass('proj-' + i )) {
+          $('.img-cont').css('background-image', 'url(../assets/img/temp' + i + '.jpg)');
+        }
+      }
     });
 
 
