0d939d65b74e1e403f0e9dd82613c21330549666
Karly Hoffman <karlyhoffman@gmail.com>
2017-10-29 11:12:55 -0500

    Navbar v2 — restructuring

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,24 +4,6 @@
 
   $(function () {
 
-    // About Page Nav -- white text
-    var pathName = window.location.pathname;
-    if (pathName == '/projects.html') {
-      $('.navbar-brand').addClass('white-copy');
-      $('.nav-link').addClass('dk');
-    } else if (pathName == '/about.html') {
-      $('.navbar-brand, footer').addClass('white-copy');
-    } else if (pathName == '/contact.html') {
-      $('.nav-link, .navbar-brand').addClass('dk');
-    } else {
-      $('.navbar-brand, footer').removeClass('white-copy');
-      $('.nav-link, .navbar-brand').removeClass('dk');
-    }
-
-
-    // pathName == '/about.html' ? $('.navbar-brand, footer').addClass('white-copy') : $('.navbar-brand, footer').removeClass('white-copy');
-    // pathName == '/contact.html' ? $('.nav-link, .navbar-brand').addClass('dk') : $('.nav-link, .navbar-brand').removeClass('dk');
-
     // Homepage project list
     var listItem = $('.project-list > li');
     $(listItem).mouseenter(function() {
@@ -38,7 +20,6 @@
         }
       }
     });
-
 
   });
 
