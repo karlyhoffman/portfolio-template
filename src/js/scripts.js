e023e7f4de28de2e5ad1e3363f57c5db5ba7f702
Karly <karlyhoffman@gmail.com>
2017-11-02 07:31:48 -0500

    Added ScrollMagic + Project page animations

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -25,6 +25,25 @@
       }
     });
 
+    // Reveal animations
+   var controller = new ScrollMagic.Controller();
+
+   $( ".reveal" ).each(function( index, elem ) {
+     var revealIn = TweenMax.fromTo(this, 0.3, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop:0, ease: Power0.easeNone });
+     var revealScene = new ScrollMagic.Scene({triggerHook: "0.72", triggerElement: this}).setTween(revealIn).addTo(controller);
+   });
+
+   $( ".fade" ).each(function( index, elem ) {
+     var fadeIn = TweenMax.fromTo(this, 0.5, {"opacity": "0"}, {"opacity": "1", ease: Power0.easeNone});
+     var fadeScene = new ScrollMagic.Scene({triggerHook: "0.95", triggerElement: this}).setTween(fadeIn).addTo(controller);
+   });
+
+   $( ".reveal-stagger" ).each(function( index, elem ) {
+     var animStaggerSubjects = $(elem).find('.reveal-elmt');
+     var fadeStaggerIn = TweenMax.staggerFromTo(animStaggerSubjects, 0.3, {"opacity": "0", marginTop:20}, {"opacity": "1", marginTop:0, ease: Power2.easeOut}, 0.2);
+     var revealStaggerScene = new ScrollMagic.Scene({triggerHook: "0.75", triggerElement: this}).setTween(fadeStaggerIn).addTo(controller);
+   });
+
   });
 
 })(jQuery, window, document);
